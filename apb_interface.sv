200ab58a5457449937b8492eb943187ac7b825a2
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2024-07-22 14:05:36 +0530

    Update apb_interface.sv

--- apb_interface.sv
+++ apb_interface.sv
@@ -1,13 +1,41 @@
+//APB interface
+
 interface apb_interface;
 
+  //CLOCK AND RESET SIGNAL
+  
   logic pclk;
   logic rst_n;
+
+  //MASTER SIGNAL
+  
   logic [31:0] paddr;
   logic psel;
   logic penable;
   logic pwrite;
   logic [31:0] pwdata;
+
+  //SLAVE SIGNAL
+  
   logic pready;
   logic [31:0] prdata;
+  logic 
  
 endinterface
+
+
+
+// interface apb_if();
+//   //Reset and clock
+//   bit PCLK;
+//   bit PRESET;
+//   bit PSEL;
+//   bit PENABLE;
+//   bit PWRITE;
+//   bit [0:3] PSTRB;
+//   logic [31:0] PADDR;
+//   logic [31:0] PWDATA;
+//   logic [31:0] PRDATA;
+//   bit PREADY;
+//   bit PSLVERR; 
+// endinterface: apb_if
