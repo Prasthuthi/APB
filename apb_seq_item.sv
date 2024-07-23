b943e90820ba84305abbc0efc515786b23ce7810
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2024-07-23 11:13:12 +0530

    Update apb_seq_item.sv

--- apb_seq_item.sv
+++ apb_seq_item.sv
@@ -6,21 +6,45 @@
 class apb_seq_item extends uvm_sequence_item;
 
     //factory registration
-  `uvm_object_utils(apb_seq_item)
+ // `uvm_object_utils(apb_seq_item)
+     `uvm_object_utils_begin(apb_seq_item)
+			`uvm_field_int(pwrite,UVM_DEFAULT)
+			`uvm_field_int(penable,UVM_DEFAULT)
+			`uvm_field_int(psel,UVM_DEFAULT)
+			`uvm_field_int(pwdata,UVM_DEFAULT)
+			`uvm_field_int(paddr,UVM_DEFAULT)
+			`uvm_field_int(pready,UVM_DEFAULT)
+			`uvm_field_int(prdata,UVM_DEFAULT)
+		`uvm_object_utils_end
   
   //typedef for READ/Write transaction type
   typedef enum {READ, WRITE} RW;
 
-  //Declaration of the signals
-  rand bit   [31:0] addr;      //Address
-  rand bit [31:0] data;     //Data - For write or read response
-  rand RW  pwrite;       //command type
-  
-  // constraint c1{addr[31:0]>=32'd0; addr[31:0] <32'd256;};
-  // constraint c2{data[31:0]>=32'd0; data[31:0] <32'd256;};
-  
+//-----------------Declaration of the signals----------------- 
+//-----------------Randomizing master signals-----------------
+
+  rand bit [31:0] paddr;
+  rand bit [31:0] pwdata;
+  rand RW pwrite;
+  rand bit psel;
+  rand bit penable;
+
+ //-----------------Slave signals-----------------
+  bit pready;
+  bit [31:0] prdata;
+
+ //-----------------Constraint-----------------------------
+
+  constraint c1{paddr inside {[0:255];}
+  constraint c2{pdata inside {[0:255];}
+
+  //class constructor
+
   function new (string name = "apb_seq_item");
     super.new(name);
   endfunction
   
 endclass
+
+
+  
